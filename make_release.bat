c29e63844dd3502cfb9027df3cb20c026233b81f
Oleg Chensky <olegchensky@users.noreply.github.com>
2021-06-16 07:56:48 +0200

    path

--- make_release.bat
+++ make_release.bat
@@ -1,3 +1,3 @@
-"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" aradar.iss
-"c:\Program Files\7-Zip\7z.exe" a -t7z -mx9 "C:\Temp\aradar.x32.7z" "D:\a-tools.com\0\A70F4593F-A711-4C1F-BEAF-FD480A8193A3\aradar.x32.exe"
-"c:\Program Files\7-Zip\7z.exe" a -t7z -mx9 "C:\Temp\aradar.x64.7z" "D:\a-tools.com\0\A70F4593F-A711-4C1F-BEAF-FD480A8193A3\aradar.x64.exe"
+:"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" aradar.iss
+"c:\Program Files\7-Zip\7z.exe" a -t7z -mx9 "C:\Temp\aradar.x32.7z" "d:\Programs\A-Radar\Win32\Release\aradar.x32.exe"
+"c:\Program Files\7-Zip\7z.exe" a -t7z -mx9 "C:\Temp\aradar.x64.7z" "d:\Programs\A-Radar\Win64\Release\aradar.x64.exe"
